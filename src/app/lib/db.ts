2a966dca824db396b1caac1730769c953cea15a3
julianty <alexanderjulianty@gmail.com>
2026-01-26 14:04:36 -0800

    Update logic for adding reminders to database

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
+import { Reminder } from "@/app/types";
 
-export async function addReminder() {
+export async function addReminder(reminder: Omit<Reminder, "id">) {
   const uri = process.env.MONGO_DB_CONNECTION_STRING!;
   if (!uri)
     throw new Error("MONGO_DB_CONNECTION_STRING env variable not configured");
@@ -11,7 +12,8 @@ export async function addReminder() {
     const db = client.db("reminders");
     const testReminders = db.collection("test-reminders");
 
-    const result = await testReminders.insertOne({ date: "01-01-26" });
+    const result = await testReminders.insertOne(reminder);
+    return result;
   } catch (e) {
     if (e instanceof Error) {
       console.error(e.message);
