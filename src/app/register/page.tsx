17c45b19f1296dce9f6d6dc4fd7dd26fdea6ae76
julianty <alexanderjulianty@gmail.com>
2026-01-26 11:43:19 -0800

    Adjust page styling

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,14 +4,17 @@ import { signIn } from "next-auth/react";
 
 export default function RegisterPage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-pink-400 via-sky-400 to-orange-400">
-      <div className="bg-white bg-opacity-80 rounded-xl shadow-lg p-8 max-w-md w-full text-center">
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <div className="rounded-xl p-8 max-w-md w-full text-center">
         <h1 className="text-3xl font-bold mb-4">Register</h1>
         <p className="mb-6">
           The app is currently in development and we only have Google as an
           OAuth provider as yet.
         </p>
-        <button onClick={() => signIn("google", { callbackUrl: "/dashboard" })}>
+        <button
+          className="bg-button p-2 rounded-lg"
+          onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+        >
           Sign in with Google
         </button>
       </div>
