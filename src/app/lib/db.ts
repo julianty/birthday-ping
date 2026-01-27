08c738196bec789dab45d751437463ecd056b46e
julianty <alexanderjulianty@gmail.com>
2026-01-27 08:03:20 -0800

    Create cached client in global scope

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -3,14 +3,23 @@ import { Reminder } from "@/app/types";
 import type { CreateReminder } from "@/app/schemas/reminder.schema";
 import * as z from "zod";
 
-export async function addReminder(reminder: CreateReminder) {
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (!global._mongoClientPromise) {
   const uri = process.env.MONGO_DB_CONNECTION_STRING!;
   if (!uri)
     throw new Error("MONGO_DB_CONNECTION_STRING env variable not configured");
   const client = new MongoClient(uri);
+  global._mongoClientPromise = client.connect();
+}
 
+const clientPromise: Promise<MongoClient> = global._mongoClientPromise;
+
+export async function addReminder(reminder: CreateReminder) {
   try {
-    await client.connect();
+    const client = await clientPromise;
     const db = client.db("reminders");
     const testReminders = db.collection("test-reminders");
 
@@ -20,7 +29,23 @@ export async function addReminder(reminder: CreateReminder) {
     if (e instanceof Error) {
       console.error(e.message);
     }
-  } finally {
-    await client.close();
+  }
+}
+
+export async function getReminders(email: string) {
+  try {
+    const client = await clientPromise;
+    const db = client.db("reminders");
+    const testReminders = db.collection("test-reminders");
+
+    const result = await testReminders.find({
+      email: email,
+    });
+
+    return result.toArray();
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+    }
   }
 }
